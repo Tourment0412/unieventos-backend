1e2928980c76b47f47d8712f99f5daa150d4cf05
Tourment0412 <94409923+Tourment0412@users.noreply.github.com>
2024-08-29 22:16:47 -0500

    Add services interfaces and package
    Add Account and Event service interface with main methods declared
    Add other services interface for the rest of the model documents classes
    Add a provional class (It´s gonna be deleted or modified later) to push the package for the implementation classes for the services

---
+++ src/main/java/co/edu/uniquindio/unieventos/services/interfaces/AccountService.java
@@ -0,0 +1,23 @@
+package co.edu.uniquindio.unieventos.services.interfaces;
+
+import co.edu.uniquindio.unieventos.dto.*;
+
+public interface AccountService {
+
+    //Tne exception should be a custom exception
+    String createAccount(CreateAccountDTO account) throws Exception;
+
+    String updateAccount(UpdateAccountDTO account) throws Exception;
+
+    String deleteAccount(String id) throws Exception;
+
+    AccountInfoDTO getInfoAccount(String id) throws Exception;
+
+    String sendRecoverPasswordCode(String email) throws Exception;
+
+    String changePassword(ChangePasswordDTO changePasswordDTO) throws Exception;
+
+    String login(LoginDTO loginDTO)throws Exception;
+
+
+}
---
+++ src/main/java/co/edu/uniquindio/unieventos/services/interfaces/EventSerivice.java
@@ -0,0 +1,16 @@
+package co.edu.uniquindio.unieventos.services.interfaces;
+
+import co.edu.uniquindio.unieventos.dto.*;
+
+import java.util.List;
+
+public interface EventSerivice {
+
+    String createEvent(CreateEventDTO createEventDTO);
+    String updateEvent(UpdateEventDTO updateEventDTO);
+    String deleteEvent(String id);
+    EventInfoDTO getInfoEvent();
+    List<EventItemDTO> listEvents();
+    //The filter depends on the requirements of the  project
+    List<EventItemDTO> filterEvents(EventFilterDTO eventFilterDTO);
+}
