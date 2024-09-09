43e749a72af3bbdf2ac63939514a069e86d6c02b
Tourment0412 <94409923+Tourment0412@users.noreply.github.com>
2024-09-09 09:30:29 -0500

    Create UserObtainedByMailPassDTO.java

---
+++ src/main/java/co/edu/uniquindio/unieventos/dto/accountdtos/UserObtainedByMailPassDTO.java
@@ -0,0 +1,8 @@
+package co.edu.uniquindio.unieventos.dto.accountdtos;
+
+public record UserObtainedByMailPassDTO(
+         String phoneNumber,
+         String address,
+         String dni,
+         String name) {
+}
