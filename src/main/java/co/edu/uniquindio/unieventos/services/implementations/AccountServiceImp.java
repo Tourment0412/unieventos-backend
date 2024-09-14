769749eda5ecc1bdebca1b6bd84701d5277ee351
Tourment0412 <94409923+Tourment0412@users.noreply.github.com>
2024-09-13 23:01:42 -0500

    Update AccountServiceImp.java

--- src/main/java/co/edu/uniquindio/unieventos/services/implementations/AccountServiceImp.java
+++ src/main/java/co/edu/uniquindio/unieventos/services/implementations/AccountServiceImp.java
@@ -15,7 +15,7 @@ import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Service
-@Transactional()
+@Transactional(rollbackFor = Exception.class)
 public class AccountServiceImp implements AccountService {
     //This repo is final because we are not going to modify the repo
     private final AccountRepo accountRepo;
@@ -26,18 +26,18 @@ public class AccountServiceImp implements AccountService {
 
     @Override
     public String createAccount(CreateAccountDTO account) throws Exception {
-        if (existsDni(account.dni())){
+        if (existsDni(account.dni())) {
             //Remember adding custom exceptions
             throw new Exception("Account with this dni already exists");
         }
 
-        if(existsEmail(account.email())){
+        if (existsEmail(account.email())) {
             throw new Exception("Account with this email already exists");
         }
 
         Account newAccount = new Account();
         //Te id on the account class is given by mongodb
-        accountRepo.save(newAccount);
+
         newAccount.setEmail(account.email());
         newAccount.setPassword(account.password());
         //The goal of this register is to be for a client (Admins directly registered on db)
@@ -56,7 +56,7 @@ public class AccountServiceImp implements AccountService {
 
         //TODO Method for mail sending for activation on account
 
-        Account accountCreated= accountRepo.save(newAccount);
+        Account accountCreated = accountRepo.save(newAccount);
         return accountCreated.getId();
     }
 
@@ -64,7 +64,7 @@ public class AccountServiceImp implements AccountService {
         return accountRepo.findAccountByEmail(email).isPresent();
     }
 
-    private boolean existsDni(String dni){
+    private boolean existsDni(String dni) {
         return accountRepo.findAccountByDni(dni).isPresent();
     }
 
@@ -100,13 +100,13 @@ public class AccountServiceImp implements AccountService {
 
         Account accountToDelete = getAccount(id);
         accountToDelete.setStatus(AccountStatus.DELETED);
-        accountRepo.save(accountToDelete);//Faltraba esta linea
+        accountRepo.save(accountToDelete);
         return "Account deleted successfully";
     }
 
     private Account getAccount(String id) throws Exception {
-        Optional<Account> accountOptional= accountRepo.findAccountById(id);
-        if(accountOptional.isEmpty()){
+        Optional<Account> accountOptional = accountRepo.findAccountById(id);
+        if (accountOptional.isEmpty()) {
             throw new Exception("Account with this id does not exist");
         }
 
@@ -132,14 +132,14 @@ public class AccountServiceImp implements AccountService {
         String validationCode = generateValidationCode();
         //TODO Send this code to the user (Account) email
 
-        account.setPasswordValidationCode(new ValidationCode(LocalDateTime.now(),validationCode));
+        account.setPasswordValidationCode(new ValidationCode(LocalDateTime.now(), validationCode));
         accountRepo.save(account);
         return "A validation code has been sent to your email, check your email, it lasts 15 minutes.";
     }
 
     private Account getAccountEmail(String email) throws Exception {
-        Optional<Account> accountOptional= accountRepo.findAccountByEmail(email);
-        if(accountOptional.isEmpty()){
+        Optional<Account> accountOptional = accountRepo.findAccountByEmail(email);
+        if (accountOptional.isEmpty()) {
             throw new Exception("This email is not registered");
         }
         return accountOptional.get();
@@ -147,23 +147,23 @@ public class AccountServiceImp implements AccountService {
 
     @Override
     public String changePassword(ChangePasswordDTO changePasswordDTO) throws Exception {
-        Optional<Account> accountOptional= accountRepo.findAccountByEmail(changePasswordDTO.email());
-        if(accountOptional.isEmpty()){
+        Optional<Account> accountOptional = accountRepo.findAccountByEmail(changePasswordDTO.email());
+        if (accountOptional.isEmpty()) {
             throw new Exception("This email is not registered");
         }
         Account account = accountOptional.get();
         ValidationCode passwordValidationCode = account.getPasswordValidationCode();
-        if(passwordValidationCode !=null){
-            if(passwordValidationCode.getCode().equals(changePasswordDTO.verificationCode())){
-                if(passwordValidationCode.getCreationDate().plusMinutes(15).isBefore(LocalDateTime.now())){
+        if (passwordValidationCode != null) {
+            if (passwordValidationCode.getCode().equals(changePasswordDTO.verificationCode())) {
+                if (passwordValidationCode.getCreationDate().plusMinutes(15).isBefore(LocalDateTime.now())) {
                     account.setPassword(changePasswordDTO.newPassword());
                     accountRepo.save(account);
-                }else{
+                } else {
                     account.setPasswordValidationCode(null);
                     accountRepo.save(account);
                     throw new Exception("This verification code has expired");
                 }
-            }else{
+            } else {
                 throw new Exception("This verification is incorrect");
             }
         }
@@ -172,11 +172,40 @@ public class AccountServiceImp implements AccountService {
 
     @Override
     public String login(LoginDTO loginDTO) throws Exception {
-        Optional<Account> accouOptional = accountRepo.validateAuthenticationData(loginDTO.email(),loginDTO.password());
-        if(accouOptional.isEmpty()){
+        Optional<Account> accouOptional = accountRepo.validateAuthenticationData(loginDTO.email(), loginDTO.password());
+        if (accouOptional.isEmpty()) {
             throw new Exception("Authentication data incorrect");
         }
         //TODO This is return is going to be a token that's going to be sent to the backend
         return "TOKEN_JWT";
+    }
+
+    public String validateRegistrationCode(ActivateAccountDTO activateAccountDTO) throws Exception {
+        Account accountObtained = getAccountEmail(activateAccountDTO.email());
+        ValidationCode registrationValidationCode = accountObtained.getRegistrationValidationCode();
+
+        if(registrationValidationCode != null) {
+            if(registrationValidationCode.getCode().equals(activateAccountDTO.registrationValidationCode())){
+                if(registrationValidationCode.getCreationDate().plusMinutes(15).isBefore(LocalDateTime.now())) {
+                    accountObtained.setStatus(AccountStatus.ACTIVE);
+                    accountRepo.save(accountObtained);
+
+                }else {
+                    throw new Exception("Registration validation code has expired");
+                }
+            }else {
+                throw new Exception("This registration validation code is incorrect");
+            }
+        }
+        return accountObtained.getId();
+    }
+
+    public String reassignValidationRegistrationCode(String email) throws Exception {
+        Account accountObtained = getAccountEmail(email);
+        ValidationCode reassignValidationCode = new ValidationCode(LocalDateTime.now(), generateValidationCode());
+        accountObtained.setRegistrationValidationCode(reassignValidationCode);
+        accountRepo.save(accountObtained);
+
+        return accountObtained.getId();
     }
 }
