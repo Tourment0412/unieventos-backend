7883f58fbc530c37ec40348ee83f243c23e4e5e1
Tourment0412 <94409923+Tourment0412@users.noreply.github.com>
2024-10-02 15:50:26 -0500

    Add treatment for Valid exceptions on DTOs

---
+++ src/main/java/co/edu/uniquindio/unieventos/dto/jwtdtos/ValidationDTO.java
@@ -0,0 +1,7 @@
+package co.edu.uniquindio.unieventos.dto.jwtdtos;
+
+public record ValidationDTO(
+        String field,
+        String message
+) {
+}
--- src/main/java/co/edu/uniquindio/unieventos/exceptions/GlobalExceptions.java
+++ src/main/java/co/edu/uniquindio/unieventos/exceptions/GlobalExceptions.java
@@ -1,9 +1,16 @@
 package co.edu.uniquindio.unieventos.exceptions;
 
 import co.edu.uniquindio.unieventos.dto.jwtdtos.MessageDTO;
+import co.edu.uniquindio.unieventos.dto.jwtdtos.ValidationDTO;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestControllerAdvice
 public class GlobalExceptions {
@@ -13,6 +20,18 @@ public class GlobalExceptions {
         return ResponseEntity.internalServerError().body(new MessageDTO<>(true, e.getMessage())
         );
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<MessageDTO<List<ValidationDTO>>> validationException(
+            MethodArgumentNotValidException ex ) {
+        List<ValidationDTO> errors = new ArrayList<>();
+        BindingResult results = ex.getBindingResult();
+        for (FieldError e: results.getFieldErrors()) {
+            errors.add( new ValidationDTO(e.getField(), e.getDefaultMessage()) );
+        }
+        return ResponseEntity.badRequest().body( new MessageDTO<>(true, errors) );
+    }
+
 
     //TODO Here should be all actions
 }
