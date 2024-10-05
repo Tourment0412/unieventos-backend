ccca34aca13129bbdf3cb5ec2443d21b59bb8fd6
Tourment0412 <94409923+Tourment0412@users.noreply.github.com>
2024-10-05 14:55:51 -0500

    Update PublicController.java

--- src/main/java/co/edu/uniquindio/unieventos/controllers/PublicController.java
+++ src/main/java/co/edu/uniquindio/unieventos/controllers/PublicController.java
@@ -5,12 +5,14 @@ import co.edu.uniquindio.unieventos.dto.eventdtos.EventInfoDTO;
 import co.edu.uniquindio.unieventos.dto.eventdtos.EventItemDTO;
 import co.edu.uniquindio.unieventos.dto.jwtdtos.MessageDTO;
 import co.edu.uniquindio.unieventos.services.interfaces.EventService;
+import co.edu.uniquindio.unieventos.services.interfaces.OrderService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,6 +20,7 @@ import java.util.List;
 public class PublicController {
 
     private final EventService eventService;
+    private  final OrderService orderService;
 
     @GetMapping("/event/get-all/{page}")
     public ResponseEntity<MessageDTO<List<EventItemDTO>>> listEventsClient(@PathVariable int page){
@@ -34,11 +37,20 @@ public class PublicController {
     //TODO (ask) Teacher has a controller to get just 1 event it is for the object? same as admin or it h
     // as to be just a basic info? I think is the DTO with the info
     @GetMapping("/event/get-info/{id}")
-    public ResponseEntity<MessageDTO<EventInfoDTO>> getInfoEvent(@PathVariable String id) throws Exception{
-        EventInfoDTO eventInfo = eventService.getInfoEvent(id);
+    public ResponseEntity<MessageDTO<EventInfoDTO>> getInfoEvenClient(@PathVariable String id) throws Exception{
+        EventInfoDTO eventInfo = eventService.getInfoEventClient(id);
         return ResponseEntity.ok(new MessageDTO<>(true,eventInfo));
     }
 
+    //TODO (ask) if this service method still should be void or return
+    // and id of the order
+    //TODO (Ask) if I can return a Map in the messageDTO (yeah bit how it's going to work or look).
+    //TODO (Ask) why is it a post if it makes modifications on an order already existent
 
-    //TODO Here I need a controller to get a notification on Order
+    @PostMapping("/order/receive-notification")
+    public ResponseEntity<MessageDTO<String>> receiveNotificationFromMercadoPago(Map<String, Object> request){
+        orderService.receiveNotificationFromMercadoPago(request);
+        return ResponseEntity.ok(new MessageDTO<>(true,"Notification received"));
+    }
+
 }
