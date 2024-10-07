c22962371f310770c00200bf858710c44c559d7f
MiguelA05 <126737880+MiguelA05@users.noreply.github.com>
2024-10-06 19:43:05 -0500

    Update CreateCouponDTO.java

--- src/main/java/co/edu/uniquindio/unieventos/dto/coupondtos/CreateCouponDTO.java
+++ src/main/java/co/edu/uniquindio/unieventos/dto/coupondtos/CreateCouponDTO.java
@@ -1,15 +1,26 @@
 package co.edu.uniquindio.unieventos.dto.coupondtos;
 
 import co.edu.uniquindio.unieventos.model.enums.CouponType;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.*;
+import org.hibernate.validator.constraints.Length;
+
 import java.time.LocalDateTime;
 
 public record CreateCouponDTO(
+        @Min(value = 0, message = "Discount must be at least 0%")
+        @Max(value = 100, message = "Discount must be at most 100%")
+        float discount,
 
-        @NotBlank float discount,
-        @NotBlank LocalDateTime expirationDate,
-        @NotBlank CouponType type,
-        @NotBlank String name
+        @Future(message = "Expiration date must be in the future")
+        @NotNull(message = "Expiration date is required")
+        LocalDateTime expirationDate,
+
+        @NotNull(message = "Coupon type is required")
+        CouponType type,
+
+        @NotBlank(message = "Coupon name cannot be empty")
+        @Length(max = 50, message = "Coupon name must not exceed 50 characters")
+        String name
 
 ) {
 }
